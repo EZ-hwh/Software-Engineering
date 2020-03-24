48b0e7cc83cb7fcb294b0e3b324f90594b7bca01
ystttttt <ystttttt@users.noreply.github.com>
2020-03-24 22:25:39 +0800

    Update settings.py
    这里有个小问题哈 改了之后再npm run serve之后打开界面应该可以看到vue的一个welcome界面

--- lifeline_app/lifeline_app/settings.py
+++ lifeline_app/lifeline_app/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'lifeline_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[os.path.join(BASE_DIR, 'frontend/dist')], #setting dirs to frontend dist
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/public')], #setting dirs to frontend dist
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
