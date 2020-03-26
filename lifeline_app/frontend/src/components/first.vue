78f3adfcbc28266bbd49eee445261b5aee78fd78
ystttttt <ystttttt@users.noreply.github.com>
2020-03-26 22:29:24 +0800

    登陆注册界面基本框架
    我又重新上传了一次 其实之前那次没啥问题 用这个文件夹直接覆盖project里面frontend的文件夹就行了 然后npm install 再npm run serve

---
+++ lifeline_app/frontend/src/components/first.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="main">
+        <div id="header">
+            <h1>LIFELINE</h1>
+        </div>
+        <div id="But">
+            <div id="buttons">
+                <CustomButton source="grinningface" size="large" message="login" @click.native="log"></CustomButton>
+                <CustomButton source="grinningface" size="large" message="register" @click.native="reg"></CustomButton>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CustomButton from "./CustomButton";
+
+    export default {
+        name: "first",
+        components: {
+            CustomButton
+        },
+        methods: {
+            log: function () {
+                this.$parent.display = 2;
+            },
+            reg: function () {
+                this.$parent.display = 3;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    @font-face {
+        font-family: 'Montserrat-ExtraBold';
+        src: url('../assets/font/Montserrat-ExtraBold.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    .main {
+        background-color: antiquewhite;
+        padding: 15% 0;
+        width: 100%;
+        height: 100%;
+        font-family: Montserrat-ExtraBold,sans-serif;
+        align-items: center;
+    }
+
+    #header {
+        font-size: 80px;
+        text-align: center;
+        height: auto;
+    }
+
+    #But {
+        margin: auto;
+        width: 186px;
+        padding: 2% 0;
+    }
+
+    #buttons {
+        display: flex;
+    }
+
+</style>
