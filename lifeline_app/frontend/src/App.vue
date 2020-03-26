1a8f21f6b41805c9273fbf0ca6dcb7e4d63db340
ystttttt <ystttttt@users.noreply.github.com>
2020-03-26 21:42:27 +0800

    登录注册界面的基本框架
    主要做了一个跳转和基本布局 美化什么的也没咋做 暂时先这么个格式 具体美化的话之后再做 大家可以先看看

--- lifeline_app/frontend/src/App.vue
+++ lifeline_app/frontend/src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <first v-if="display===1"></first>
+        <Login v-else-if="display===2"></Login>
+        <Register v-else-if="display===3"></Register>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Register from "./components/Register";
+    import first from "./components/first";
+    import Login from "./components/Login";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                display: 1,
+                name: "",
+                password: ""
+            }
+        },
+        components: {
+            first,
+            Register,
+            Login
+        },
+        watch: {
+            display: function (newDisplay, oldDisplay) {
+                console.log(newDisplay, oldDisplay);
+            },
+            name: function (newName, oldName) {
+                console.log(newName, oldName);
+            },
+            password: function (newPass, oldPass) {
+                console.log(newPass, oldPass);
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding: 0px;
+        margin: 0px;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        border: hidden;
+    }
 </style>
