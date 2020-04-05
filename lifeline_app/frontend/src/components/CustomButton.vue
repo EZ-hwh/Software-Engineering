f72e6db9f64cf9b85f4a450d08195133dd583829
ystttttt <ystttttt@users.noreply.github.com>
2020-04-05 20:22:53 +0800

    更新多页面的结构
    请务必看一下“看我！！！”的那个md

---
+++ lifeline_app/frontend/src/components/CustomButton.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="but">
+        <img :src="getImage" :style="getStyle()">
+        <p id="button_tag">{{message}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CustomButton",
+        props: {
+            source: String,
+            size: String,
+            message: String
+        },
+        methods: {
+            getStyle: function () {
+                if (this.size === 'small') {
+                    return {
+                        width: "15px"
+                    }
+                } else if (this.size === 'median') {
+                    return {
+                        width: "30px"
+                    }
+                } else return {
+                    width: "45px"
+                }
+            }
+        },
+        computed: {
+            getImage: function () {
+                console.log('../assets/images/' + this.source + '.png');
+                return require('../assets/images/' + this.source + '.png');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    @font-face {
+        font-family: 'Montserrat-ExtraBold';
+        src: url('../assets/fonts/Montserrat-ExtraBold.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    #button_tag{
+        text-align: center;
+        margin: auto;
+    }
+    .but {
+        font-family: Montserrat-ExtraBold, sans-serif;
+        margin: 20px auto 0;
+        font-size: 15px;
+        width: 100px;
+        height: 70px;
+        text-align: center;
+    }
+</style>
---
+++ lifeline_app/frontend/src/components/page-header.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  components: {},
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    items: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
+}
+</script>
+
+<template>
+  <!-- start page title -->
+  <div class="row">
+    <div class="col-12">
+      <div class="page-title-box">
+        <div class="page-title-right">
+          <b-breadcrumb
+            :items="items"
+            class="m-0"
+          ></b-breadcrumb>
+        </div>
+        <h4 class="page-title">{{ title }}</h4>
+      </div>
+    </div>
+  </div>
+  <!-- end page title -->
+</template>
