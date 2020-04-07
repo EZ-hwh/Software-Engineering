0498fe5311d31e4f0ebb83775769b19331020916
Riki Du <duyixian19990123@hotmail.com>
2020-04-07 12:36:28 +0800

    部分数据接口

--- lifeline_app/lifeline_app/settings.py
+++ lifeline_app/lifeline_app/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware', #solving CORS ERROR
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
