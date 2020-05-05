606e09a95660cf22ce3c969e1ee1785697b9ad05
ystttttt <ystttttt@users.noreply.github.com>
2020-05-05 21:21:46 +0800

    add email verification

--- lifeline_app/frontend/src/views/register/Register.vue
+++ lifeline_app/frontend/src/views/register/Register.vue
@@ -4,8 +4,8 @@
             <header>
                 <h1>LifeLine</h1>
             </header>
-            <div v-show="check">
-                <p class="sec">
+            <div v-show="check===1">
+                <div class="sec">
                     <svg width="100%" height="100%">
                         <symbol id="s-text">
                             <text text-anchor="middle"
@@ -22,20 +22,50 @@
                         <use xlink:href="#s-text" class="text"
                         ></use>
                     </svg>
-
-                        <input v-model="name" placeholder="Your name?" type="text" maxlength="11">
-
-                    <CustomButton source="grinningface" size="large" message="设置暗号=3="
+                    <div>
+                        <Poptip trigger="focus">
+                            <input v-model="name" placeholder="Your name?" type="text" maxlength="11"
+                                   style="padding-top: 4%">
+                            <div slot="content">{{ name }}</div>
+                        </Poptip>
+                        <p class="tip">不可以超过11个字哦！</p>
+                        <p class="tip">只能由字母、数字、下划线组成哦！</p>
+                    </div>
+                    <CustomButton source="grinningface" size="large" message="验证邮箱"
                                   @click.native="next_page"></CustomButton>
-                </p>
-
+                </div>
 
                 <p>
                     <CustomButton source="grinningface" size="large" message="我走错了:("
                                   @click.native="back_to_main"></CustomButton>
                 </p>
             </div>
-            <div v-show="!check">
+            <div v-show="check===2">
+                <div class="sec">
+                    <p>Email check</p>
+                    <div>
+                        <Poptip trigger="focus">
+                            <input v-model="email" placeholder="Your email?" type="text" style="padding-left: 5%">
+                            <div slot="content">{{ name }}</div>
+                        </Poptip>
+                    </div>
+                    <CustomButton source="grinningface" size="large" message="Get Code!"
+                                  @click.native="get_email"></CustomButton>
+                </div>
+                <CustomButton source="grinningface" size="large" message="Wrong name!"
+                              @click.native="back"></CustomButton>
+            </div>
+            <div v-show="check===3">
+                <div class="sec">
+                    <p>Code Check</p>
+                    <input v-model="code" placeholder="code?" type="text" style="padding-left: 5%">
+                    <CustomButton source="grinningface" size="large" message="设置暗号"
+                                  @click.native="next_page"></CustomButton>
+                </div>
+                <CustomButton source="grinningface" size="large" message="Wrong email!"
+                              @click.native="check_code"></CustomButton>
+            </div>
+            <div v-show="check===4">
                 <div class="sec2">
                     {{password_message}}
                     <div id="pass_input">
@@ -46,8 +76,8 @@
                                   @click.native="Register"></CustomButton>
                 </div>
                 <p>
-                    <CustomButton source="grinningface" size="large" message="名字写错了"
-                                  @click.native="back_to_name"></CustomButton>
+                    <CustomButton source="grinningface" size="large" message="错了回主页"
+                                  @click.native="back_to_main"></CustomButton>
                 </p>
             </div>
         </div>
@@ -64,12 +94,14 @@
         },
         data() {
             return {
-                name_message: 'Nice to meet you, ',
                 password_message: '不看不看',
                 name: "",
                 password: "",
+                email: "",
+                code: "",
+                ret: "",
                 password_again: "",
-                check: true,
+                check: 1,
                 response: null
             }
         },
@@ -79,13 +111,48 @@
                 // this.$router.go(-1);
             },
             next_page: function () {
-                this.check = false;
+                this.check += 1;
             },
-            back_to_name: function () {
-                this.check = true;
+            get_email: function () {
+                this.$ajax({
+                    // TODO: 发邮箱验证码
+                    method: 'post',
+                    url: '/getcode/',
+                    params: {
+                        // name: this.name,
+                        email: this.email,
+                        type: 'code',
+                        contentType: 'application/json',
+                        dataType: 'json',
+                    }
+                }).then(response => {
+                    if (response.data.flag === true) {
+                        //TODO：弹窗
+                        this.check += 1;
+                        this.ret = response.data.code;
+                        console.log("发送成功");
+                    } else {
+                        //TODO:错误处理
+                        console.log(response.data.error_msg);
+                    }
+                })
+            },
+            check_code: function () {
+                if(this.code===this.ret)
+                {
+                    this.check += 1;
+                        console.log("验证码正确");
+                }
+                else{
+                    console.log("验证码错误");
+                }
+            },
+            back: function () {
+                this.check -= 1;
             },
             Register: function () {
                 this.$ajax({
+                    // TODO:
                     method: 'post',
                     url: '/register/',
                     params: {
@@ -97,7 +164,7 @@
                         dataType: 'json',
                     }
                 }).then(response => {
-                    if (response.data.flag == true) {
+                    if (response.data.flag === true) {
                         window.location.href = "/login";
                     } else {
                         console.log(response.data.error_msg);
@@ -119,7 +186,9 @@
                         }
                     )
             },
-        },
+            //TODO:姓名密码的检测
+        }
+        ,
         watch: {
             check: function (newCheck, oldCheck) {
                 console.log(newCheck, oldCheck);
@@ -154,7 +223,7 @@
     input {
         padding: 0 0 0 2%;
         background: transparent;
-        width: 55%;
+        width: 100%;
         height: 100px;
         border: 0px;
         outline: #7B7988;
@@ -164,6 +233,11 @@
 
     input::placeholder {
         font-size: 45px;
+    }
+
+    .tip {
+        padding: 0 4%;
+        font-size: small;
     }
 
     .sec {
