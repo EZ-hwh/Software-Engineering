da512a9bb5161b599475f32757fa8d3aa91c1774
ystttttt <ystttttt@users.noreply.github.com>
2020-05-17 10:34:11 +0800

    lessons interface backend done

--- lifeline_app/backend/data.py
+++ lifeline_app/backend/data.py
@@ -1,3 +1,53 @@
 def get_course_sample_data():
-    ret = [{'lesson': [{'name': '并发理论 Concurrency Models', 'index': 1}, {'name': 'COMP130011.01 算法设计与分析 Algorithm Design and Analysis', 'index': 2}, {'name': 'COMP130012.01 计算机体系结构 Computer Architecture', 'index': 3}, {'name': 'COMP130015.02 软件工程 Software Engineering', 'index': 4}]}, {'lesson': [{'name': 'CHIN110029.01 华语电影评析 On Chinese Language Films', 'index': 1}, {'name': 'COMP130006.02 概率论与数理统计 Probability Theory and Mathematical Statistics', 'index': 2}, {'name': 'COMP130021.01 信息安全原理 Principles of Information Security', 'index': 3}, {'name': 'COMP130110.01 操作系统 Operating Systems', 'index': 4}, {'name': 'COMP130136.02 计算机网络 Computer Networks', 'index': 5}, {'name': 'COMP130141.01 自然语言处理 Natural Language Processing', 'index': 6}, {'name': 'ENGL110045 学术英语（科学技术） Academic English for Science and Technology', 'index': 7}]}, {'lesson': [{'name': 'COMP130005.01 代数结构与数理逻辑 Algebra Structure and Mathematical Logic', 'index': 1}, {'name': 'COMP130010.01 数据库引论 Introduction to Database Systems', 'index': 2}, {'name': 'COMP130137.01 模式识别与机器学习 Pattern Recognition & Machine Learning', 'index': 3}, {'name': 'COMP130144.01 计算机系统基础（下） Introduction to Computer Systems II', 'index': 4}, {'name': 'COMP130148.01 数据挖掘技术 Data Mining', 'index': 5}, {'name': 'ENGL110061.28 英语论说文写作 Essay Writing', 'index': 6}, {'name': 'PEDU110078.25 排球 Volleyball', 'index': 7}, {'name': 'PHIL119050.02 《理想国》导读 Introduction to The Republic', 'index': 8}, {'name': 'PTSS110005.01 应用伦理学 Applied Ethics', 'index': 9}, {'name': 'PTSS110061.04 形势与政策IV Situation and Policy IV', 'index': 10}]}, {'lesson': [{'name': '968.033.1.01 概率论与数理统计 Probability Theory and Mathematical Statistics', 'index': 1}, {'name': '968.037.1.03 微观经济学 Microeconomics', 'index': 2}, {'name': '969.001.1.02 会计学 Accounting', 'index': 3}, {'name': 'COMP130002.01 数字逻辑与部件设计 Digital Logic and Component Design', 'index': 4}, {'name': 'COMP130003.01 数字逻辑与部件设计实验 Experiments of Digital Logic and Components Design', 'index': 5}, {'name': 'COMP130004.01 数据结构 Data Structure', 'index': 6}, {'name': 'COMP130143.01 计算机系统基础（上） Introduction to Computer Systems I', 'index': 7}, {'name': 'COMP130149.01 集合与图论 Set Theory and Graph Theory', 'index': 8}, {'name': 'PEDU110078.25 排球 Volleyball', 'index': 9}, {'name': 'PTSS110060.04 形势与政策III Situation and Policy III', 'index': 10}, {'name': 'PTSS110072.12 毛泽东思想和中国特色社会主义理论体系概论A An Introduction to MaoZedong Thought and the Theoretical System of Socialism with Chinese CharacterA', 'index': 11}, {'name': 'PTSS110073.12 毛泽东思想和中国特色社会主义理论体系概论B An Introduction to MaoZedong Thought and the Theoretical System of Socialism with Chinese CharacterB', 'index': 12}]}, {'lesson': [{'name': 'COMP130135.04 面向对象程序设计 Object Oriented Programming', 'index': 1}, {'name': 'ENGL110012.04 英语视听 Audiovisual English', 'index': 2}, {'name': 'INFO120002.01 模拟电子学基础 Basics of Analog Electronics', 'index': 3}, {'name': 'MATH120017.04 数学分析BII Mathematical Analysis BII', 'index': 4}, {'name': 'NDEC110002.07 军事理论 Military Theory', 'index': 5}, {'name': 'PEDU110077.06 网球 Tennis', 'index': 6}, {'name': 'PHYS120014.12 大学物理B(下) College Physics B II', 'index': 7}, {'name': 'PHYS120015.05 基础物理实验 Fundamental Physics Experiments', 'index': 8}, {'name': 'PTSS110008.11 中国近现代史纲要 Outline of Modern Chinese History', 'index': 9}, {'name': 'PTSS110059.02 形势与政策II Situation and Policy II', 'index': 10}]}, {'lesson': []}]
+    ret = {"course_table": [{'lesson': [{'name': '并发理论 Concurrency Models', 'index': 1},
+                                {'name': 'COMP130011.01 算法设计与分析 Algorithm Design and Analysis', 'index': 2},
+                                {'name': 'COMP130012.01 计算机体系结构 Computer Architecture', 'index': 3},
+                                {'name': 'COMP130015.02 软件工程 Software Engineering', 'index': 4}]}, {
+                        'lesson': [{'name': 'CHIN110029.01 华语电影评析 On Chinese Language Films', 'index': 1},
+                                   {'name': 'COMP130006.02 概率论与数理统计 Probability Theory and Mathematical Statistics',
+                                    'index': 2},
+                                   {'name': 'COMP130021.01 信息安全原理 Principles of Information Security', 'index': 3},
+                                   {'name': 'COMP130110.01 操作系统 Operating Systems', 'index': 4},
+                                   {'name': 'COMP130136.02 计算机网络 Computer Networks', 'index': 5},
+                                   {'name': 'COMP130141.01 自然语言处理 Natural Language Processing', 'index': 6},
+                                   {'name': 'ENGL110045 学术英语（科学技术） Academic English for Science and Technology',
+                                    'index': 7}]}, {
+                        'lesson': [
+                            {'name': 'COMP130005.01 代数结构与数理逻辑 Algebra Structure and Mathematical Logic', 'index': 1},
+                            {'name': 'COMP130010.01 数据库引论 Introduction to Database Systems', 'index': 2},
+                            {'name': 'COMP130137.01 模式识别与机器学习 Pattern Recognition & Machine Learning', 'index': 3},
+                            {'name': 'COMP130144.01 计算机系统基础（下） Introduction to Computer Systems II', 'index': 4},
+                            {'name': 'COMP130148.01 数据挖掘技术 Data Mining', 'index': 5},
+                            {'name': 'ENGL110061.28 英语论说文写作 Essay Writing', 'index': 6},
+                            {'name': 'PEDU110078.25 排球 Volleyball', 'index': 7},
+                            {'name': 'PHIL119050.02 《理想国》导读 Introduction to The Republic', 'index': 8},
+                            {'name': 'PTSS110005.01 应用伦理学 Applied Ethics', 'index': 9},
+                            {'name': 'PTSS110061.04 形势与政策IV Situation and Policy IV', 'index': 10}]}, {
+                        'lesson': [{'name': '968.033.1.01 概率论与数理统计 Probability Theory and Mathematical Statistics',
+                                    'index': 1},
+                                   {'name': '968.037.1.03 微观经济学 Microeconomics', 'index': 2},
+                                   {'name': '969.001.1.02 会计学 Accounting', 'index': 3},
+                                   {'name': 'COMP130002.01 数字逻辑与部件设计 Digital Logic and Component Design', 'index': 4},
+                                   {
+                                       'name': 'COMP130003.01 数字逻辑与部件设计实验 Experiments of Digital Logic and Components Design',
+                                       'index': 5}, {'name': 'COMP130004.01 数据结构 Data Structure', 'index': 6},
+                                   {'name': 'COMP130143.01 计算机系统基础（上） Introduction to Computer Systems I', 'index': 7},
+                                   {'name': 'COMP130149.01 集合与图论 Set Theory and Graph Theory', 'index': 8},
+                                   {'name': 'PEDU110078.25 排球 Volleyball', 'index': 9},
+                                   {'name': 'PTSS110060.04 形势与政策III Situation and Policy III', 'index': 10}, {
+                                       'name': 'PTSS110072.12 毛泽东思想和中国特色社会主义理论体系概论A An Introduction to MaoZedong Thought and the Theoretical System of Socialism with Chinese CharacterA',
+                                       'index': 11}, {
+                                       'name': 'PTSS110073.12 毛泽东思想和中国特色社会主义理论体系概论B An Introduction to MaoZedong Thought and the Theoretical System of Socialism with Chinese CharacterB',
+                                       'index': 12}]}, {
+                        'lesson': [{'name': 'COMP130135.04 面向对象程序设计 Object Oriented Programming', 'index': 1},
+                                   {'name': 'ENGL110012.04 英语视听 Audiovisual English', 'index': 2},
+                                   {'name': 'INFO120002.01 模拟电子学基础 Basics of Analog Electronics', 'index': 3},
+                                   {'name': 'MATH120017.04 数学分析BII Mathematical Analysis BII', 'index': 4},
+                                   {'name': 'NDEC110002.07 军事理论 Military Theory', 'index': 5},
+                                   {'name': 'PEDU110077.06 网球 Tennis', 'index': 6},
+                                   {'name': 'PHYS120014.12 大学物理B(下) College Physics B II', 'index': 7},
+                                   {'name': 'PHYS120015.05 基础物理实验 Fundamental Physics Experiments', 'index': 8},
+                                   {'name': 'PTSS110008.11 中国近现代史纲要 Outline of Modern Chinese History', 'index': 9},
+                                   {'name': 'PTSS110059.02 形势与政策II Situation and Policy II', 'index': 10}]}
+                    ]}
     return ret
