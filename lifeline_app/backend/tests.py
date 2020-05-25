8a2e3b85ae7c6aa0603625c5c526ea58075f5a8d
ystttttt <ystttttt@users.noreply.github.com>
2020-05-25 20:59:01 +0800

    test

--- lifeline_app/backend/tests.py
+++ lifeline_app/backend/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+
+class MyAppTests(TestCase):
+    def setUp(self):
+        super(MyAppTests, self).setUp()
+        self.client = Client(enforce_csrf_checks=True)
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
