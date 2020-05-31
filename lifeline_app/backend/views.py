dc6d01dd85ab6b5814d05760ab0a703d3e1ddb7a
Riki Du <duyixian19990123@hotmail.com>
2020-05-31 10:37:59 +0800

    debug

--- lifeline_app/backend/views.py
+++ lifeline_app/backend/views.py
@@ -54,7 +54,7 @@ def register_account(request):
         else:
             ret["flag"] = True
             user = User.objects.create_user(username=name, password=pwd, email=email)
-            login(redirect, user)
+            login(request, user)
             account = Account.objects.create(user=user, nickname="None")
         return HttpResponse(json.dumps(ret), content_type="application/json")
     '''
@@ -131,7 +131,7 @@ def personal(request):
     if not request.user.is_authenticated:
         return redirect('/login')
     if request.method == 'GET':
-        return render(request, 'personal.htmpersonl')
+        return render(request, 'personal.html')
 
 
 @csrf_exempt
@@ -369,6 +369,7 @@ def check_todolist(request):
             ret["error_msg"] = "Wrong status!"
             return JsonResponse(ret)
         Todo.status = status
+        Todo.save()
         ret["flag"] = True
         return JsonResponse(ret)
 
@@ -393,11 +394,11 @@ def add_ddl(request):
             return JsonResponse(ret)
         """
         account = Account.objects.get(user = request.user)
-        todolist = Todolist.objects.create()
+        todolist = Todolist(name=request.GET["name"])
         todolist.account = account
-        todolist.name = request.GET["name"]
         todolist.deadline_time = request.GET["time"]
         todolist.description = request.GET["description"]
+        todolist.save()
         ret = {}
         ret["flag"] = True
         return JsonResponse(ret)
@@ -445,6 +446,7 @@ def elearning_register(request):
     if not request.user.is_authenticated:
         return redirect('/login_page/')
     if request.method == 'POST':
+        print("elearning!")
         name = request.GET["name"]
         password = request.GET["password"]
         account = Account.objects.get(user = request.user)
@@ -477,6 +479,7 @@ def information(request):
         account.addr = request.GET["addr"]
         account.phone = request.GET["phone"]
         account.description = request.GET["mail"]
+        account.save()
         ret = {"flag":True}
         return JsonResponse(ret)
         
@@ -488,6 +491,7 @@ def picture(request):
     if request.method == 'POST':
         account = Account.objects.get(user = request.user)
         account.picture = request.GET["pic"]
+        account.save()
         ret = {"flag": True}
         return JsonResponse(ret)
         
