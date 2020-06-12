b19b79cbf059e12e5a0aacefc46e4634ac9b30fc
Stephyuka <tian_rui_0512@outlook.com>
2020-06-12 21:08:35 +0800

    redo

--- lifeline_app/frontend/src/assets/js/pages/draggable.init.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".sortable").sortable({connectWith:".sortable",items:".card-draggable",revert:!0,placeholder:"card-draggable-placeholder",forcePlaceholderSize:!0,opacity:.77,cursor:"move"})});
